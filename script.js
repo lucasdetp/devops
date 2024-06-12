156aad4e0e477923927cf957e8734a41b7bd5d45
Luluww 👽 <detplucas@gmail.com>
2024-06-12 16:17:34 +0200

    test sans xss

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const userInput = urlParams.get('userInput');
-    document.getElementById('output').innerHTML = userInput;
-});
